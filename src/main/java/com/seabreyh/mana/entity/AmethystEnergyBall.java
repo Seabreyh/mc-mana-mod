160d7b29eca72e291b424e2b9e1abc78733e4f5c
Brendan Weirtz <brendanweirtz@gmail.com>
2022-09-05 20:47:14 -0400

    Fixed amethyst staff fire

--- src/main/java/com/seabreyh/mana/entity/AmethystEnergyBall.java
+++ src/main/java/com/seabreyh/mana/entity/AmethystEnergyBall.java
@@ -1,8 +1,9 @@
 package com.seabreyh.mana.entity;
 
-import com.seabreyh.mana.ManaMod;
 import com.seabreyh.mana.particle.ManaParticles;
 import com.seabreyh.mana.registry.ManaEntities;
+
+import javax.annotation.Nonnull;
 
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.protocol.Packet;
@@ -25,21 +26,21 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 
-import javax.annotation.Nonnull;
-
 public class AmethystEnergyBall extends ThrowableProjectile {
     private int life;
     private int explosionPower = 2;
+    private boolean fireCharged;
 
     public AmethystEnergyBall(Level world, LivingEntity player) {
         super(ManaEntities.AMETHYST_ENERGY_BALL.get(), player, world);
+        
         double xPos = player.getX();
         double yPos = player.getEyeY() - 0.1D;
         double zPos = player.getZ();
+        
         this.setPos(xPos, yPos, zPos);
         this.noPhysics = true;
         this.life = 0;
-        ManaMod.LOGGER.debug("### AMETHYST ENERGY BALL");
     }
 
     public AmethystEnergyBall(EntityType<? extends AmethystEnergyBall> p_37391_, Level p_37392_) {
@@ -59,6 +60,7 @@ public class AmethystEnergyBall extends ThrowableProjectile {
                         this.random.nextGaussian() * (double) 0.0075F * (double) p_37270_,
                         this.random.nextGaussian() * (double) 0.0075F * (double) p_37270_)
                 .scale((double) p_37269_);
+
         this.setDeltaMovement(vec3);
         double d0 = vec3.horizontalDistance();
         this.setYRot((float) (Mth.atan2(vec3.x, vec3.z) * (double) (180F / (float) Math.PI)));
@@ -68,11 +70,11 @@ public class AmethystEnergyBall extends ThrowableProjectile {
     }
 
     @Override
-    public void shootFromRotation(Entity player, float p_37253_, float p_37254_, float p_37255_, float p_37256_,
-            float p_37257_) {
+    public void shootFromRotation(Entity player, float p_37253_, float p_37254_, float p_37255_, float p_37256_, float p_37257_) {
         float f = -Mth.sin(p_37254_ * ((float) Math.PI / 180F)) * Mth.cos(p_37253_ * ((float) Math.PI / 180F));
         float f1 = -Mth.sin((p_37253_ + p_37255_) * ((float) Math.PI / 180F));
         float f2 = Mth.cos(p_37254_ * ((float) Math.PI / 180F)) * Mth.cos(p_37253_ * ((float) Math.PI / 180F));
+        
         this.shoot((double) f, (double) f1, (double) f2, p_37256_, p_37257_);
         Vec3 vec3 = player.getDeltaMovement();
         this.setDeltaMovement(this.getDeltaMovement().add(vec3.x, 0.0F, vec3.z));
@@ -80,6 +82,7 @@ public class AmethystEnergyBall extends ThrowableProjectile {
 
     @Override
     protected void defineSynchedData() {
+        //
     }
 
     protected SoundEvent getPloofSound() {
@@ -97,17 +100,33 @@ public class AmethystEnergyBall extends ThrowableProjectile {
     public void tick() {
         super.tick();
 
-        this.playSound(this.getPloofSound(), 2F, 3F);
+        if (this.isOnFire()) {
+            if (!this.fireCharged) {
+                fireCharged = true;
+                this.playSound(SoundEvents.BLAZE_SHOOT, 3.0F, 0.3F);
+                this.playSound(SoundEvents.FIRECHARGE_USE, 3.0F, 1F);
+                this.playSound(SoundEvents.PUFFER_FISH_BLOW_OUT, 3.0F, 1F);
+            }
+        }
+
+        if (this.fireCharged) {
+            this.playSound(this.getPloofSound(), 2F, 0.2F);
+        } else {
+            this.playSound(this.getPloofSound(), 2F, 3F);
+        }
 
         ++this.life;
 
         boolean flag = this.noPhysics;
         Vec3 vec3 = this.getDeltaMovement();
+
         // (double)3 changes the underwater speed.
         this.setPosRaw(this.getX(), this.getY() + vec3.y * 0.015D * (double) 3, this.getZ());
+
         if (this.level.isClientSide) {
             this.yOld = this.getY();
         }
+
         // (double)3 changes the underwater speed.
         double d0 = 0.05D * (double) 3;
         this.setDeltaMovement(this.getDeltaMovement().scale(0.75D).add(vec3.normalize().scale(d0)));
@@ -120,17 +139,28 @@ public class AmethystEnergyBall extends ThrowableProjectile {
             }
         } else {
             for (int i = 0; i < 4; ++i) {
+                if (fireCharged) {
+                    this.level.addParticle(ManaParticles.MAGIC_PLOOM_PARTICLE_FIRE.get(), this.getX(),
+                        this.getY(), this.getZ(),
+                        this.random.nextGaussian() * 0.2D, this.random.nextGaussian() * 0.2D,
+                        this.random.nextGaussian() * 0.2D);
 
-                this.level.addParticle(ManaParticles.MAGIC_PLOOM_PARTICLE_DEFAULT.get(), this.getX(),
+                    this.level.addParticle(ParticleTypes.FLAME, this.getX(),
+                        this.getY(), this.getZ(),
+                        this.random.nextGaussian() * 0D, this.random.nextGaussian() * 0.02D,
+                        this.random.nextGaussian() * 0.05D);
+                } else {
+                    this.level.addParticle(ManaParticles.MAGIC_PLOOM_PARTICLE_DEFAULT.get(),
+                        this.getX(),
                         this.getY(), this.getZ(),
                         this.random.nextGaussian() * 0.1D, this.random.nextGaussian() * 0.1D,
                         this.random.nextGaussian() * 0.1D);
+                }
 
                 vec3 = this.getDeltaMovement();
                 double d5 = vec3.x;
                 double d6 = vec3.y;
                 double d1 = vec3.z;
-
                 double d7 = this.getX() + d5;
                 double d2 = this.getY() + d6;
                 double d3 = this.getZ() + d1;
@@ -161,13 +191,10 @@ public class AmethystEnergyBall extends ThrowableProjectile {
     protected void onHitBlock(BlockHitResult hitBlock) {
         BlockState blockstate = this.level.getBlockState(hitBlock.getBlockPos().above());
 
-        if (this.wasOnFire || blockstate.getBlock() == Blocks.FIRE
-                || blockstate.getBlock() == Blocks.SOUL_FIRE) {
+        if (fireCharged || this.wasOnFire || blockstate.getBlock() == Blocks.FIRE || blockstate.getBlock() == Blocks.SOUL_FIRE) {
             if (!this.level.isClientSide) {
-                boolean flag = net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(this.level,
-                        this.getOwner());
-                this.level.explode((Entity) null, this.getX(), this.getY(), this.getZ(), (float) this.explosionPower,
-                        flag, flag ? Explosion.BlockInteraction.DESTROY : Explosion.BlockInteraction.NONE);
+                boolean flag = net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(this.level, this.getOwner());
+                this.level.explode((Entity) null, this.getX(), this.getY(), this.getZ(), (float) this.explosionPower, flag, flag ? Explosion.BlockInteraction.DESTROY : Explosion.BlockInteraction.NONE);
                 this.discard();
             }
         } else {
@@ -175,10 +202,8 @@ public class AmethystEnergyBall extends ThrowableProjectile {
         }
 
         if (!this.level.isClientSide) { // qty spread velocity
-            ((ServerLevel) this.level).sendParticles(ParticleTypes.FLASH, this.getX(), this.getY(), this.getZ(), 1, 0D,
-                    0D, 0D, 0D);
-            ((ServerLevel) this.level).sendParticles(ParticleTypes.END_ROD, this.getX(), this.getY(), this.getZ(), 20,
-                    1D, 1D, 1D, 0.3D);
+            ((ServerLevel) this.level).sendParticles(ParticleTypes.FLASH, this.getX(), this.getY(), this.getZ(), 1, 0D, 0D, 0D, 0D);
+            ((ServerLevel) this.level).sendParticles(ParticleTypes.END_ROD, this.getX(), this.getY(), this.getZ(), 20, 1D, 1D, 1D, 0.3D);
         }
 
         super.onHitBlock(hitBlock);
@@ -201,9 +226,15 @@ public class AmethystEnergyBall extends ThrowableProjectile {
             this.playSound(this.getHitSound(), 1.2F, 1.5F);
 
             if (!this.level.isClientSide) {
-                ((ServerLevel) this.level).sendParticles(ParticleTypes.CRIT, this.getX(), this.getY(), this.getZ(), 20,
-                        0.15D,
-                        0.15D, 0.15D, 0.2D);
+                ((ServerLevel) this.level).sendParticles(ParticleTypes.CRIT, this.getX(), this.getY(), this.getZ(), 20, 0.15D, 0.15D, 0.15D, 0.2D);
+            }
+
+            if (fireCharged || this.wasOnFire) {
+                if (!this.level.isClientSide) {
+                    boolean flag2 = net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(this.level, this.getOwner());
+                    this.level.explode((Entity) null, this.getX(), this.getY(), this.getZ(), (float) this.explosionPower, flag2, flag2 ? Explosion.BlockInteraction.DESTROY : Explosion.BlockInteraction.NONE);
+                    this.discard();
+                }
             }
 
             this.discard();
